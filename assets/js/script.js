93ed3129ae5f95d7a3eefb4137b1414decdbf169
sweinkauf <scott.weinkauf29@gmail.com>
2022-10-18 23:37:46 -0500

    cleaning up Java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,47 @@
-
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
    
-    $(".saveBtn").on("click", function () {
+$(".saveBtn").on("click", function () {
         
-        console.log(this);
-        var text = $(this).siblings(".description").val(); 
-        var time = $(this).parent().attr("id"); 
+    console.log(this);
+    var text = $(this).siblings(".description").val(); 
+    var time = $(this).parent().attr("id"); 
 
-        
-        localStorage.setItem(time, text);
-    })
+    localStorage.setItem(time, text);
+})
   
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
 
-    function hourTracker() {
+function hourTracker() {
         
-        var currentHour = moment().hour(); 
+    var currentHour = moment().hour(); 
+ 
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
 
-      
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( blockHour, currentHour)
-
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (blockHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-    hourTracker(); 
-
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker(); 
