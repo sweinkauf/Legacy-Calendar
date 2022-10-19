9efd3d3c79c41b9bf7e9943176fb93505f5c63ce
sweinkauf <scott.weinkauf29@gmail.com>
2022-10-18 23:11:41 -0500

    changed HTML and added JAVA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,51 @@
-currentDay = moment().format("MMM Do, YYYY");
-$("#currentDay").text(currentDay);
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+   
+    $(".saveBtn").on("click", function () {
+        
+        console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
+
+        
+        localStorage.setItem(time, text);
+    })
+  
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+
+    function hourTracker() {
+        
+        var currentHour = moment().hour(); 
+
+      
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker(); 
+
